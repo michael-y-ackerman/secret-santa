afe56b8a6dff4e346e9fc0f1b0ecaf30461c3db6
Michael Ackerman <michaelackerman314@gmail.com>
2025-12-14 05:31:08 -0500

    updated frontend to use deployment url instead of localhost.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,6 +8,6 @@ const URL = JSON.stringify("https://api.secretsanta.michaelyackerman.com/api")
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   define: {
-    'import.meta.env.VITE_BACKEND_BASE_URL': JSON.stringify('http://localhost:3000/api')
+    'import.meta.env.VITE_BACKEND_BASE_URL': URL,
   },
 })
